6f46b85743cd8fdcad565fb809e6d52b9b79413f
ydnadev <andy@ydna.dev>
2023-05-22 21:41:11 -0700

    add dates fix
    Useful for cleaning dates to ISO 8061 date formats.

---
+++ dates_fix.pl
@@ -0,0 +1,178 @@
+#! use perl
+
+use warnings;
+use strict;
+
+my $inputFile = $ARGV[0];
+my $outputFile = "dates_fixed-".$inputFile;
+
+open(IN,"<$inputFile") || print "can't open $inputFile\n";
+open(OUT,">>$outputFile") || print "can't open $outputFile\n";
+
+
+for $_(<IN>){
+    my $line = $_;
+    #print "$line\n";
+    chomp $line;
+
+    my @array = split(",",$line);
+    my @data_array;
+
+    for $_(@array){
+        my $var = $_;
+        my $var_new;
+
+        # 01/02/2012
+        if ($var =~ m/(..)(\/|-)(..)(\/|-)(....)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9][0-9]/){
+                if ($d =~ m/[0-9][0-9]/){
+                    $var_new = "$y-$m-$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 10/5/2012
+        elsif ($var =~ m/(..)(\/|-)(.)(\/|-)(....)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9][0-9]/){
+                if ($d =~ m/[0-9]/){
+                    $var_new = "$y-$m-0$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 5/10/2012
+        elsif ($var =~ m/(.)(\/|-)(..)(\/|-)(....)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9]/){
+                if ($d =~ m/[0-9][0-9]/){
+                    $var_new = "$y-0$m-$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 5/1/2012
+        elsif ($var =~ m/(.)(\/|-)(.)(\/|-)(....)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9]/){
+                if ($d =~ m/[0-9]/){
+                    $var_new = "$y-0$m-0$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 01/02/12
+        elsif($var =~ m/(..)(\/|-)(..)(\/|-)(..)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9][0-9]/){
+                if ($d =~ m/[0-9][0-9]/){
+                    $var_new = "20$y-$m-$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 10/5/12
+        elsif($var =~ m/(..)(\/|-)(.)(\/|-)(..)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9][0-9]/){
+                if ($d =~ m/[0-9]/){
+                    $var_new = "20$y-$m-0$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 5/10/12
+        elsif ($var =~ m/(.)(\/|-)(..)(\/|-)(..)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9]/){
+                if ($d =~ m/[0-9][0-9]/){
+                    $var_new = "20$y-0$m-$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 5/1/12
+        elsif ($var =~ m/(.)(\/|-)(.)(\/|-)(..)/){
+            my ($m,$d,$y) = ($1,$3,$5);
+            if ($m =~ m/[0-9]/){
+                if ($d =~ m/[0-9]/){
+                    $var_new = "20$y-0$m-0$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 10-Apr-12
+        elsif ($var =~ m/(..)(\/|-)(...)(\/|-)(..)/){
+            my ($d,$m,$y) = ($1,$3,$5);
+            if ($m =~ m/([A-Z]|[a-z]{3})/){
+                $var =~ s/(J|j)an/01/g;
+                $var =~ s/(F|f)eb/02/g;
+                $var =~ s/(M|m)ar/03/g;
+                $var =~ s/(A|a)pr/04/g;
+                $var =~ s/(M|m)ay/05/g;
+                $var =~ s/(J|j)un/06/g;
+                $var =~ s/(J|j)ul/07/g;
+                $var =~ s/(A|a)ug/08/g;
+                $var =~ s/(S|s)ep/09/g;
+                $var =~ s/(O|o)ct/10/g;
+                $var =~ s/(N|n)ov/11/g;
+                $var =~ s/(D|d)ec/12/g;
+                if ($d =~ m/[0-9][0-9]/){
+                    $var_new = "20$y-$m-$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 1-Apr-12 or 1/Apr/12
+        elsif ($var =~ m/(.)(\/|-)(...)(\/|-)([0-9][0-9])/){
+            my ($d,$m,$y) = ($1,$3,$5);
+            if ($m =~ m/([A-Z]|[a-z]{3})/){
+                $var =~ s/(J|j)an/01/g;
+                $var =~ s/(F|f)eb/02/g;
+                $var =~ s/(M|m)ar/03/g;
+                $var =~ s/(A|a)pr/04/g;
+                $var =~ s/(M|m)ay/05/g;
+                $var =~ s/(J|j)un/06/g;
+                $var =~ s/(J|j)ul/07/g;
+                $var =~ s/(A|a)ug/08/g;
+                $var =~ s/(S|s)ep/09/g;
+                $var =~ s/(O|o)ct/10/g;
+                $var =~ s/(N|n)ov/11/g;
+                $var =~ s/(D|d)ec/12/g;
+                if ($d =~ m/[0-9]/){
+                    $var_new = "20$y-$m-0$d";
+                    push(@data_array,$var_new);
+                    print "$var_new";
+                }
+            }
+        }
+        # 2020/07/11
+        elsif ($var =~ m/([0-9][0-9][0-9][0-9])(\/|-)([0-9][0-9])(\/|-)([0-9][0-9])/){
+            my ($d,$m,$y) = ($5,$3,$1);
+            $var_new = "$y-$m-$d";
+                    push(@data_array,$var_new);
+            print "$var_new\n";
+        }
+        else {
+            push(@data_array,$var);
+            print "$var";
+        }
+        print ",";
+    }
+    my $new_line = join(',',@data_array);
+    print OUT "$new_line\n";
+    print "\n";
+}
+close IN;
+close OUT;
+print "New file --> $outputFile\n";

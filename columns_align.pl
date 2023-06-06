ec11eb412272d05fa4a6f742fb48d552a7fd7a18
Andy Richardson <arichardson@sdzwa.org>
2023-06-06 14:48:58 -0700

    add documentation

--- columns_align.pl
+++ columns_align.pl
@@ -4,13 +4,17 @@
 use warnings;
 use strict;
 
+# call 2 files from ARGV
 my $file1 = $ARGV[0];
 my $file2 = $ARGV[1];
 
+# line count var
 my $lc = 0;
 
+# row vars
 my ($row1, $row2);
 
+# read first file
 open(F1,"<$file1");
 for $_(<F1>){
     last if $lc == 1;
@@ -19,7 +23,10 @@ for $_(<F1>){
     $lc++;
 }
 
+# reset line count
 $lc = 0;
+
+# read second file
 open(F2,"<$file2");
 for $_(<F2>){
     last if $lc == 1;
@@ -28,19 +35,22 @@ for $_(<F2>){
     $lc++;
 }
 
+# sort headers
 my @d1 = split("\",\"", $row1);
 my @row1_sorted = sort(@d1);
 my $r1 = join("\",\"", @row1_sorted);
-
 my @d2 = split("\",\"", $row2);
 my @row2_sorted = sort(@d2);
 my $r2 = join("\",\"", @row2_sorted);
 
+# map header arrays to hashes
 my %head1 = map { $_ => 1 } @row1_sorted;
 my %head2 = map { $_ => 1 } @row2_sorted;
+
+# use hashes to compare to eachother
+# print missing columns for each file
 my @m1;
 my @m2;
-
 print "Fields missing from $file2\n\tfound in $file1:\n";
 foreach my $var(keys %head1){
     if(exists($head2{$var})){ 
@@ -55,7 +65,6 @@ for $_(@m1_s){
     print "$_\n";
 }
 print "---\n";
-
 print "Fields missing from $file1\n\tfound in $file2:\n";
 foreach my $var(keys %head2){
     if(exists($head1{$var})){ 
